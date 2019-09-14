b9b4a18b4e4b081f1266684e0f94f57eeec18701
League Student <league@iMac-23.attlocal.net>
2019-09-14 11:28:02 -0700

    stuff

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,7 +8,6 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
-		
 		return "";
 	}
 
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -8,7 +8,6 @@ public class MixedCapsString extends SpecialString{
 
 	@Override
 	public String funkifyText(String s) {
-		
 		return "";
 	}
 
