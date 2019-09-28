652e6c305e2853a8698c597fb8e7e36c7f67f10f
League Student <league@iMac-14.attlocal.net>
2019-09-28 11:27:09 -0700

    save

---
+++ src/_00_Text_Funkifier/CustomString.java
@@ -0,0 +1,38 @@
+package _00_Text_Funkifier;
+
+import java.util.ArrayList;
+
+public class CustomString extends SpecialString{
+
+	public CustomString(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String funkifyText(String s) {
+		// TODO Auto-generated method stub
+		String converted="";
+		Character saved;
+		ArrayList<Character> chars=new ArrayList<Character>();
+		for(int i=0; i<s.length()-1; i++) {
+			if(s.charAt(i)!=32) {
+			chars.add(s.charAt(i));
+			}
+		}
+		for(int j=1; j<chars.size(); j++) {
+			for(int k=chars.size(); k>0; k--) {	
+				if(chars.get(j-1).compareTo(chars.get(j))>0) {
+					saved=chars.get(j-1);
+					chars.set(j-1, chars.get(j));
+					chars.set(j, saved);
+				}
+			}
+		}
+		for(int i=0; i<chars.size(); i++) {
+			converted+=chars.get(i);
+		}
+		return converted;
+	}
+
+}
--- src/_00_Text_Funkifier/_00_TestSpecialString.java
+++ src/_00_Text_Funkifier/_00_TestSpecialString.java
@@ -38,6 +38,8 @@ public class _00_TestSpecialString {
 	//   the JUnitTest case below to test your customized Special String.
 	@Test
 	public void testCustomString() {
-	
+	SpecialString cs=new CustomString("This is a very custom test");
+	System.out.println(cs);
+	assertEquals("aceehiimrssstttuvy", cs.toString());
 	}
 }
